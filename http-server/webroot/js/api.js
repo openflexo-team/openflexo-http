9425b9d4ae2a7b1ed8afb77d5cdc3a479449040d
Jean-Charles <jeancharles.roger@me.com>
2017-02-08 14:34:58 +0100

    MEDIUM / Adds ta presentation

--- http-server/webroot/js/api.js
+++ http-server/webroot/js/api.js
@@ -1,18 +1,41 @@
 "use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var openflexo;
 (function (openflexo) {
-    var Center = (function () {
+    var Description = (function () {
+        function Description() {
+        }
+        return Description;
+    }());
+    openflexo.Description = Description;
+    var Center = (function (_super) {
+        __extends(Center, _super);
         function Center() {
+            return _super !== null && _super.apply(this, arguments) || this;
         }
         return Center;
-    }());
+    }(Description));
     openflexo.Center = Center;
-    var ResourceCenter = (function () {
+    var ResourceCenter = (function (_super) {
+        __extends(ResourceCenter, _super);
         function ResourceCenter() {
+            return _super !== null && _super.apply(this, arguments) || this;
         }
         return ResourceCenter;
-    }());
+    }(Description));
     openflexo.ResourceCenter = ResourceCenter;
+    var TechnologyAdapter = (function (_super) {
+        __extends(TechnologyAdapter, _super);
+        function TechnologyAdapter() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return TechnologyAdapter;
+    }(Description));
+    openflexo.TechnologyAdapter = TechnologyAdapter;
     function error(url) {
         console.log("Error can't access " + url + '", check that it exists and is accessible');
     }
@@ -24,6 +47,10 @@ var openflexo;
         call("http://localhost:8080/resource", callback);
     }
     openflexo.resources = resources;
+    function technologyAdapters(callback) {
+        call("http://localhost:8080/ta", callback);
+    }
+    openflexo.technologyAdapters = technologyAdapters;
     function call(url, callback) {
         var request = new XMLHttpRequest();
         request.open("get", url);
@@ -39,29 +66,57 @@ var openflexo;
     }
     openflexo.call = call;
 })(openflexo || (openflexo = {}));
+function getDataUrl(element) {
+    var current = element;
+    var dataUrl = current.getAttribute("data-url");
+    while (dataUrl === null) {
+        current = current.parentElement;
+        if (!current) {
+            break;
+        }
+        dataUrl = current.getAttribute("data-url");
+    }
+    return dataUrl;
+}
+function updateDetails(event) {
+    var target = event.target;
+    var dataUrl = getDataUrl(target);
+    if (dataUrl) {
+        console.log("Get info from " + dataUrl);
+        openflexo.call(dataUrl, function (result) {
+            target.appendChild(createDescriptionElement(result));
+        });
+    }
+    event.preventDefault();
+}
+function createDescriptionElement(source) {
+    var description = document.createElement("div");
+    description.setAttribute("data-url", source.url);
+    //description.onclick = updateDetails;
+    var a = document.createElement("a");
+    a.href = source.url;
+    a.text = source.name;
+    description.appendChild(a);
+    return description;
+}
+openflexo.technologyAdapters(function (tas) {
+    var div = document.querySelector("#tas");
+    for (var _i = 0, tas_1 = tas; _i < tas_1.length; _i++) {
+        var ta = tas_1[_i];
+        div.appendChild(createDescriptionElement(ta));
+    }
+});
 openflexo.centers(function (centers) {
     var div = document.querySelector("#centers");
     for (var _i = 0, centers_1 = centers; _i < centers_1.length; _i++) {
         var center = centers_1[_i];
-        var child = document.createElement("div");
-        var a = document.createElement("a");
-        a.href = center.url;
-        a.text = center.name;
-        child.appendChild(a);
-        div.appendChild(child);
-        console.log("center");
+        div.appendChild(createDescriptionElement(center));
     }
 });
 openflexo.resources(function (resources) {
     var div = document.querySelector("#resources");
     for (var _i = 0, resources_1 = resources; _i < resources_1.length; _i++) {
         var resource = resources_1[_i];
-        var child = document.createElement("div");
-        var a = document.createElement("a");
-        a.href = resource.url;
-        a.text = resource.name;
-        child.appendChild(a);
-        div.appendChild(child);
-        console.log("resource");
+        div.appendChild(createDescriptionElement(resource));
     }
 });
--- http-server/webroot/js/api.ts
+++ http-server/webroot/js/api.ts
@@ -3,20 +3,23 @@
 
 module openflexo {
     
-    export class Center {
+    export class Description {
         name: string;
         id: string;
-        uri: string;
         url: string;
     }
 
-    export class ResourceCenter {
-        name: string;
-        id: string;
+    export class Center extends Description {
         uri: string;
-        url: string;
+    }
+
+    export class ResourceCenter extends Description{
+        uri: string;
         resourceCenterId: string;
         resourceCenterUrl: string;
+    }
+
+    export class TechnologyAdapter extends Description {
     }
 
     function error(url: string) {
@@ -29,6 +32,10 @@ module openflexo {
 
     export function resources(callback: (resources: ResourceCenter[]) => void) {
         call("http://localhost:8080/resource", callback);
+    }
+
+    export function technologyAdapters(callback: (tas: TechnologyAdapter[]) => void) {
+        call("http://localhost:8080/ta", callback);
     }
 
     export function call<T>(url: string, callback: (result: T) => void) {
@@ -46,28 +53,59 @@ module openflexo {
     }
 }
 
+function getDataUrl(element : HTMLElement) {
+    let current = element;
+    let dataUrl = current.getAttribute("data-url");
+    while (dataUrl === null) {
+        current = current.parentElement;
+        if (!current) { break; }
+        dataUrl = current.getAttribute("data-url");
+    }
+    return dataUrl;
+}
+
+function updateDetails(event: MouseEvent) {
+    let target = <HTMLElement>event.target;
+    let dataUrl = getDataUrl(target);
+    if (dataUrl) {
+        console.log("Get info from " + dataUrl);
+        openflexo.call(dataUrl, (result: openflexo.Description) => {
+            target.appendChild(createDescriptionElement(result));
+        });
+    }
+    event.preventDefault();
+}
+
+function createDescriptionElement(source: openflexo.Description) {
+    let description = <HTMLDivElement> document.createElement("div");
+    description.setAttribute("data-url", source.url);
+    //description.onclick = updateDetails;
+    
+    let a = <HTMLAnchorElement> document.createElement("a");
+    a.href = source.url;
+    a.text = source.name;
+
+    description.appendChild(a);
+    return description;
+}
+
+openflexo.technologyAdapters((tas) => {
+    let div = document.querySelector("#tas");
+    for (let ta of tas) {
+        div.appendChild(createDescriptionElement(ta));
+    }
+});
+
 openflexo.centers((centers) => {
     let div = document.querySelector("#centers");
     for (let center of centers) {
-        let child = <HTMLDivElement> document.createElement("div");
-        let a = <HTMLAnchorElement> document.createElement("a");
-        a.href = center.url;
-        a.text = center.name;
-        child.appendChild(a);
-        div.appendChild(child);
-        console.log("center")
+        div.appendChild(createDescriptionElement(center));
     }
 });
 
 openflexo.resources((resources) => {
     let div = document.querySelector("#resources");
     for (let resource of resources) {
-        let child = <HTMLDivElement> document.createElement("div");
-        let a = <HTMLAnchorElement> document.createElement("a");
-        a.href = resource.url;
-        a.text = resource.name;
-        child.appendChild(a);
-        div.appendChild(child);
-        console.log("resource")
+        div.appendChild(createDescriptionElement(resource));
     }
 });
