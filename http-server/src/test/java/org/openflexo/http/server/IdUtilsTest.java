93958768426635615da734555dc1ef0589e0fbab
Jean-Charles <jeancharles.roger@me.com>
2017-02-11 12:53:15 +0100

    LOW / Adds test

---
+++ http-server/src/test/java/org/openflexo/http/server/IdUtilsTest.java
@@ -0,0 +1,24 @@
+package org.openflexo.http.server;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for IdUtils
+ */
+public class IdUtilsTest {
+
+	@Test
+	public void testId1() {
+		checkUriEncoding("http://openflexo.org//VirtualModel/ViewPoint1.viewpoint/ViewPoint1.xml");
+		checkUriEncoding("http://openflexo.org//VirtualModel/lmlkmlkmlkmlk");
+		checkUriEncoding("http://openflexo.org//VirtualModel/&é'(§");
+		checkUriEncoding("http://openflexo.org//VirtualModel/%%ùù$$");
+	}
+
+	private void checkUriEncoding(String uri) {
+		String encoded = IdUtils.encoreUri(uri);
+		Assert.assertNotNull(encoded);
+		Assert.assertEquals(uri, IdUtils.decodeId(encoded));
+	}
+}
