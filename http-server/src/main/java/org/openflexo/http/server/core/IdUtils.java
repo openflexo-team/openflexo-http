f2be1191f7859476a4406cd51813aec54315aca9
Jean-Charles <jeancharles.roger@me.com>
2017-05-10 11:44:23 +0200

    Adds id cleaner

--- http-server/src/main/java/org/openflexo/http/server/core/IdUtils.java
+++ http-server/src/main/java/org/openflexo/http/server/core/IdUtils.java
@@ -6,6 +6,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.openflexo.foundation.technologyadapter.TechnologyAdapter;
 
 /**
  * Created by charlie on 07/02/2017.
@@ -30,6 +34,43 @@ public class IdUtils {
 			/* can't happen */
 			return url;
 		}
+	}
+
+
+	private static Set<Character> validCharacters = Stream.of(
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '$', '-', '_', '.', '+', '!', '*', '\'', '(', ')'
+		).collect(Collectors.toSet());
+
+	/**
+	 * Cleans given raw id to construct a sanitized id for REST apis.
+	 * Only Alphanumerics [0-9a-zA-Z] and special characters $-_.+!*'(), are allowed.
+	 *
+	 * A raw id will always give the same sanitized id but it's not possible to reconstruct
+	 * the raw id from the sanitized one.
+	 *
+	 * @param rawId id to clean, cant be null
+	 * @return a sanitized id, never null,never empty
+	 * @throws NullPointerException if raw id is null
+	 * @throws IllegalArgumentException if clean id is empty.
+	 */
+	public static String sanitiseId(String rawId) {
+		if (rawId == null) throw new NullPointerException();
+
+		StringBuilder id = new StringBuilder();
+		for (int i = 0; i < rawId.length(); i++) {
+			char current = rawId.charAt(i);
+			if (validCharacters.contains(current)) {
+				id.append(Character.toLowerCase(current));
+			}
+		}
+		if (id.length() == 0) throw new IllegalArgumentException("Sanitized id is empty from '" + rawId +"'");
+		return id.toString();
+	}
+
+	public static String getTechnologyAdapterId(TechnologyAdapter adapter) {
+		return sanitiseId(adapter.getIdentifier());
 	}
 
 }
