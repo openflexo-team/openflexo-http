b816075191377e0bf59a2a96881c58608c9f42ff
Sylvain Guérin <sylvain@openflexo.org>
2017-07-24 09:14:09 +0200

    MEDIUM / Working on converting support from former *.viewpoint

--- http-connector/src/main/java/org/openflexo/http/connector/rm/AccessPointResourceFactory.java
+++ http-connector/src/main/java/org/openflexo/http/connector/rm/AccessPointResourceFactory.java
@@ -16,40 +16,46 @@ import org.openflexo.toolbox.StringUtils;
  *
  */
 public class AccessPointResourceFactory
-    extends PamelaResourceFactory<AccessPointResource, AccessPoint, HttpTechnologyAdapter, AccessPointFactory>
-{
+		extends PamelaResourceFactory<AccessPointResource, AccessPoint, HttpTechnologyAdapter, AccessPointFactory> {
 
-    public static final String URL_EXTENSION = ".url";
+	public static final String URL_EXTENSION = ".url";
 
-    public AccessPointResourceFactory() throws ModelDefinitionException {
-        super(AccessPointResource.class);
-    }
+	public AccessPointResourceFactory() throws ModelDefinitionException {
+		super(AccessPointResource.class);
+	}
 
-    @Override
-    public AccessPointFactory makeResourceDataFactory(AccessPointResource resource, TechnologyContextManager<HttpTechnologyAdapter> technologyContextManager) throws ModelDefinitionException {
-        FlexoEditingContext editingContext = technologyContextManager.getServiceManager().getEditingContext();
-        return new AccessPointFactory(resource, editingContext);
-    }
+	@Override
+	public AccessPointFactory makeResourceDataFactory(AccessPointResource resource,
+			TechnologyContextManager<HttpTechnologyAdapter> technologyContextManager) throws ModelDefinitionException {
+		FlexoEditingContext editingContext = technologyContextManager.getServiceManager().getEditingContext();
+		return new AccessPointFactory(resource, editingContext);
+	}
 
-    @Override
-    public AccessPoint makeEmptyResourceData(AccessPointResource resource) {
+	@Override
+	public AccessPoint makeEmptyResourceData(AccessPointResource resource) {
 		return resource.getFactory().makeEmptyModel();
 	}
 
-    @Override
-    public <I> boolean isValidArtefact(I serializationArtefact, FlexoResourceCenter<I> resourceCenter) {
-        String name = resourceCenter.retrieveName(serializationArtefact);
-        return StringUtils.hasExtension(name, URL_EXTENSION);
-    }
+	@Override
+	public <I> boolean isValidArtefact(I serializationArtefact, FlexoResourceCenter<I> resourceCenter) {
+		String name = resourceCenter.retrieveName(serializationArtefact);
+		return StringUtils.hasExtension(name, URL_EXTENSION);
+	}
 
-    public <I> AccessPointResource makeAccessPointResource(String baseName, RepositoryFolder<AccessPointResource, I> folder, TechnologyContextManager<HttpTechnologyAdapter> technologyContextManager)
-        throws SaveResourceException, ModelDefinitionException {
+	@Override
+	public <I> I getConvertableArtefact(I serializationArtefact, FlexoResourceCenter<I> resourceCenter) {
+		return null;
+	}
 
-        FlexoResourceCenter<I> rc = folder.getResourceRepository().getResourceCenter();
-        String artefactName = baseName.endsWith(URL_EXTENSION) ? baseName : baseName + URL_EXTENSION;
-        I serializationArtefact = rc.createEntry(artefactName, folder.getSerializationArtefact());
-        AccessPointResource newAccessPointResource = makeResource(serializationArtefact, rc, technologyContextManager, true);
+	public <I> AccessPointResource makeAccessPointResource(String baseName, RepositoryFolder<AccessPointResource, I> folder,
+			TechnologyContextManager<HttpTechnologyAdapter> technologyContextManager)
+			throws SaveResourceException, ModelDefinitionException {
 
-        return newAccessPointResource;
-    }
+		FlexoResourceCenter<I> rc = folder.getResourceRepository().getResourceCenter();
+		String artefactName = baseName.endsWith(URL_EXTENSION) ? baseName : baseName + URL_EXTENSION;
+		I serializationArtefact = rc.createEntry(artefactName, folder.getSerializationArtefact());
+		AccessPointResource newAccessPointResource = makeResource(serializationArtefact, rc, technologyContextManager, true);
+
+		return newAccessPointResource;
+	}
 }
