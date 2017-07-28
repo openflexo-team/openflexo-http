9dd990b8ab2344a911d0bfa44927c94b83fdcad2
Sylvain Guérin <sylvain@openflexo.org>
2017-07-28 10:43:13 +0200

    MEDIUM / Rebase with last 1.8.1 + fixed compile issues

--- http-server/src/main/java/org/openflexo/http/server/fml/FMLRouteComplement.java
+++ http-server/src/main/java/org/openflexo/http/server/fml/FMLRouteComplement.java
@@ -2,11 +2,10 @@ package org.openflexo.http.server.fml;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.openflexo.foundation.fml.FMLTechnologyAdapter;
-import org.openflexo.foundation.fml.ViewPoint;
-import org.openflexo.foundation.fml.ViewPointLibrary;
 import org.openflexo.foundation.fml.VirtualModel;
-import org.openflexo.foundation.fml.rm.ViewPointResource;
+import org.openflexo.foundation.fml.VirtualModelLibrary;
 import org.openflexo.foundation.fml.rm.VirtualModelResource;
 import org.openflexo.http.server.HttpService;
 import org.openflexo.http.server.core.TechnologyAdapterRouteComplement;
@@ -28,33 +27,28 @@ public class FMLRouteComplement implements TechnologyAdapterRouteComplement {
 	@Override
 	public void initialize(HttpService service, TechnologyAdapterRouteService taRouteService) throws Exception {
 		technologyAdapter = service.getServiceManager().getTechnologyAdapterService().getTechnologyAdapter(getTechnologyAdapterClass());
-		ViewPointLibrary viewPointLibrary = technologyAdapter.getViewPointLibrary();
+		VirtualModelLibrary virtualModelLibrary = technologyAdapter.getVirtualModelLibrary();
 		TechnologyAdapterRouteService taService = service.getTechnologyAdapterRestService();
 
+		// TODO: Jean-Charles, could you check this ???
+
 		// Adds pamela rest service for ViewPoint resources
-		PamelaResourceRestService<ViewPoint, ViewPointResource> viewPointConverter = new PamelaResourceRestService<>(
-			"/viewpoint",
-			viewPointLibrary::getViewPoints,
-			viewPointLibrary::getViewPointResource,
-			ViewPointResource.class, taService
-		);
-		viewPointConverter.setPostLoader((ViewPoint vp) -> vp.loadVirtualModelsWhenUnloaded());
+		PamelaResourceRestService<VirtualModel, VirtualModelResource> viewPointConverter = new PamelaResourceRestService<>("/virtualmodel",
+				virtualModelLibrary::getVirtualModels, virtualModelLibrary::getVirtualModelResource, VirtualModelResource.class, taService);
+		viewPointConverter.setPostLoader((VirtualModel vp) -> vp.loadContainedVirtualModelsWhenUnloaded());
 		taRouteService.registerPamelaResourceRestService(technologyAdapter, viewPointConverter);
 
 		// Adds pamela rest service for VirtualModel resources
 		PamelaResourceRestService<VirtualModel, VirtualModelResource> virtualModelConverter = new PamelaResourceRestService<>(
-			"/virtualmodel",
-			this::getViewResources,
-			viewPointLibrary::getVirtualModelResource,
-			VirtualModelResource.class, taService
-		);
+				"/containedvirtualmodel", this::getViewResources, virtualModelLibrary::getVirtualModelResource, VirtualModelResource.class,
+				taService);
 		taRouteService.registerPamelaResourceRestService(technologyAdapter, virtualModelConverter);
 	}
 
 	private List<VirtualModelResource> getViewResources() {
 		ArrayList<VirtualModelResource> result = new ArrayList<>();
-		for (ViewPointResource viewPointResource : technologyAdapter.getViewPointLibrary().getViewPoints()) {
-			result.addAll(viewPointResource.getVirtualModelResources());
+		for (VirtualModelResource viewPointResource : technologyAdapter.getVirtualModelLibrary().getVirtualModels()) {
+			result.addAll(viewPointResource.getContainedVirtualModelResources());
 		}
 		return result;
 	}
