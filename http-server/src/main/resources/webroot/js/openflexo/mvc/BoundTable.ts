b9f5229efaecdc62b8beb34ff277a2ad2a029099
Jean-Charles <jeancharles.roger@me.com>
2017-08-17 19:30:25 +0200

    MEDIUM / Corrects update support for list bindings

--- http-server/src/main/resources/webroot/js/openflexo/mvc/BoundTable.ts
+++ http-server/src/main/resources/webroot/js/openflexo/mvc/BoundTable.ts
@@ -13,6 +13,8 @@ export class BoundTable implements Component {
 
     table : Table;
 
+    lines: BoundTableLine[] = [];
+    
     container: HTMLTableElement;
 
     constructor(
@@ -38,33 +40,80 @@ export class BoundTable implements Component {
         }
 
         this.api.evaluate<Description<any>[]>(this.elements).then(elements => this.updateList(elements));
-        this.api.addChangeListener(this.elements, elements => this.updateList(elements));
+        this.api.addChangeListener(this.elements, event => this.updateList(event.value));
 
         this.container = this.table.container;
     }    
 
+    refresh() {
+        this.api.evaluate<Description<any>[]>(this.elements).then(elements => this.updateList(elements));
+    }
+
     private updateList(elements: Description<any>[]) {
         let body = this.table.body;
-        body.removeLines();
 
-        for (let element of elements) {
-            let line = new TableLine();
-            for (let column of this.columns) {
-                let component = column.component(this.api, element);
-                let cell = new TableCell(component);
-                line.addCell(cell);
-            }
-            body.addLine(line);
+        let lineCount = 0;
+        let elementCount = 0;
+
+        // checks elements with current lines
+        while (lineCount < this.lines.length && elementCount < elements.length) {
+            let line = this.lines[lineCount];
+            let element = elements[elementCount];
+
+            if (line.url !== element.url) {
+                // line if different from current element
+                // removes the line current line
+                this.removeLine(lineCount);
+            } else {
+                // line is the same checks next
+                lineCount += 1;
+                elementCount += 1;
+            } 
+        }
+    
+        // removes the rest of the lines if any
+        while (lineCount < this.lines.length) {
+            this.removeLine(lineCount);
+        }
+
+        // adds the rest of elements if any
+        while (elementCount < elements.length) {
+            this.addLine(elements[elementCount]);
+            elementCount += 1;
         }
     }
+
+    private addLine(element: Description<any>) {
+        let line = new TableLine();
+        for (let column of this.columns) {
+            let component = column.component(this.api, element);
+            let cell = new TableCell(component);
+            line.addCell(cell);
+        }
+
+        this.lines.push(new BoundTableLine(element.url, line));
+        this.table.body.addLine(line);
+    }
+
+    private removeLine(index: number) {
+        let line = this.lines[index];
+        this.lines.splice(index, 1);
+        this.table.body.removeLine(line.line);
+    }
+
 }
 
 
-export class BoundColumn {
-    
+export class BoundColumn {   
     constructor(
         public name: string,
         public component: (api: Api, element: Description<any>) => Component|PhrasingCategory 
     ) { }
+}
 
+class BoundTableLine {
+    constructor(
+        public url: string,
+        public line: TableLine
+    ) { }
 }
