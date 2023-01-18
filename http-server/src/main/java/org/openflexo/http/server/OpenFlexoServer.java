fc0fc66e9d3499d2952e5d3296cad5fb1a45e70c
Ihab Benamer <benamerihab25@gmail.com>
2023-01-18 15:27:16 +0100

    MEDIUM / add resource preloading code

--- http-server/src/main/java/org/openflexo/http/server/OpenFlexoServer.java
+++ http-server/src/main/java/org/openflexo/http/server/OpenFlexoServer.java
@@ -2,10 +2,15 @@ package org.openflexo.http.server;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ServiceLoader;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
@@ -16,7 +21,9 @@ import org.openflexo.foundation.FlexoServiceManager;
 import org.openflexo.foundation.fml.FMLTechnologyAdapter;
 import org.openflexo.foundation.fml.rt.FMLRTTechnologyAdapter;
 import org.openflexo.foundation.resource.DirectoryResourceCenter;
+import org.openflexo.foundation.resource.FlexoResource;
 import org.openflexo.foundation.resource.FlexoResourceCenterService;
+import org.openflexo.foundation.resource.ResourceManager;
 import org.openflexo.foundation.technologyadapter.TechnologyAdapterService;
 import org.openflexo.foundation.utils.ProjectInitializerException;
 import org.openflexo.foundation.utils.ProjectLoadingCancelledException;
@@ -65,6 +72,8 @@ public class OpenFlexoServer extends AbstractVerticle {
 				
 		public static FlexoEditor globalEditor;
 
+		public String preloadFilePath = null;
+
 	}
 
 	public static FlexoServiceManager createServiceManager(Options options) {
@@ -86,6 +95,7 @@ public class OpenFlexoServer extends AbstractVerticle {
 		usage.append("- --host host: server host.\n");
 		usage.append("- --center path: resource center to register (may have several).\n");
 		usage.append("- --project path: path for a project to open (may have several).\n");
+		usage.append("- --preload file: Path with resources to load at startup (one URI per line).\n");
 		usage.append("\n");
 		usage.append("\n");
 
@@ -146,6 +156,15 @@ public class OpenFlexoServer extends AbstractVerticle {
 						System.exit(1);
 					}
 					break;
+				case "--preload":
+					if (i + 1 < args.length) {
+						options.preloadFilePath = args[++i];
+					}
+					else {
+						System.err.println("Option " + arg + " needs an argument.");
+						System.exit(1);
+					}
+					break;
 				default: {
 					if (arg.length() >= 2 && arg.charAt(0) == '-' && arg.charAt(1) != '-') {
 						for (int j = 1; j < arg.length(); j++) {
@@ -195,6 +214,69 @@ public class OpenFlexoServer extends AbstractVerticle {
 				Options.globalEditor = manager.getProjectLoaderService().loadProject(new File(path));
 			} catch (ProjectLoadingCancelledException | ProjectInitializerException e) {
 				System.err.println("[ERROR] Can't load project '" + path + "'.");
+			}
+		}
+
+		if(options.preloadFilePath != null) {
+			startupPreload(options.preloadFilePath);
+		}
+	}
+
+	private static void startupPreload(String preloadFilePath) {
+		// TEST FLX - preload specific resources
+
+		List<String> resourcesUrisToPreload = new ArrayList<>();
+
+
+		// Load preload URIs from file
+		try {
+			Path path = Paths.get(preloadFilePath);
+			Stream<String> lines = Files.lines(path);
+			resourcesUrisToPreload = lines.collect(Collectors.toList());
+		} catch (Exception e) {
+			System.err.println("[ERROR] Cannot read the preload file : " + preloadFilePath);
+			e.printStackTrace();
+			System.exit(127);
+		}
+
+		/*
+
+		String resourcesUrisToPreload [] = {"http://www.openflexo.org/projects/2021/12/sse4space_1638887944201.prj/Lifecycle.fml",
+										"http:://telindus.lu/SSE4Space/SSE4Space/RBAC.fml",
+										"http:://telindus.lu/SSE4Space/SSE4Space/MinioVersionModel.fml",
+										"http:://telindus.lu/SSE4Space/SSE4Space"};
+		*/
+
+		ResourceManager resourceManager = Options.globalEditor.getServiceManager().getResourceManager();
+		List<FlexoResource<?>> priorityList = new ArrayList<>();
+
+		// Check if resource exist
+		for(String resourceUri : resourcesUrisToPreload) {
+			FlexoResource currentRes = resourceManager.getResource(resourceUri);
+
+			if(currentRes != null) {
+				priorityList.add(currentRes);
+			} else {
+				System.err.println("[ERROR] A resource to preload does not exists in the context : " + resourceUri);
+				System.exit(127);
+			}
+		}
+
+		// Preload priority resources
+		preloadResourcesInOrder(priorityList);
+
+		// Preload all others resources
+		preloadResourcesInOrder(resourceManager.getRegisteredResources());
+	}
+
+	private static void preloadResourcesInOrder(List<FlexoResource<?>> listResources) {
+		for(FlexoResource<?> r : listResources) {
+			try {
+				System.out.println("Preloading resource : " + r.getName());
+				r.loadResourceData();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
