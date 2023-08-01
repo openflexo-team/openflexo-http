c0b9891cc00c358fb65e08f0e835a90dd29afd6d
Ihab Benamer <benamerihab25@gmail.com>
2023-08-01 11:59:40 +0200

    MEDIUM / add new LSP Client

---
+++ http-server/src/main/resources/webroot/LSPClient/assets/_.contribution-49d36299.js
@@ -0,0 +1,1 @@
+class o{get layoutInfos(){return this._layoutInfos}constructor(){this._layoutInfos=[]}registerKeyboardLayout(t){this._layoutInfos.push(t)}}o.INSTANCE=new o;export{o as K};
---
+++ http-server/src/main/resources/webroot/LSPClient/assets/snippets-f9c56857.js
@@ -0,0 +1,1 @@
+import{nM as r,uM as e,k as t,nN as n,cy as a,jA as c}from"./index-1e6bb64c.js";import{a as s}from"./snippetsService-6fee1d43.js";import"./languageService-f7a8f4bc.js";n(async i=>{i.get(a).when(2).then(()=>{c.get(e)})});function v(){return{...r(),[e.toString()]:new t(s)}}export{v as default};
