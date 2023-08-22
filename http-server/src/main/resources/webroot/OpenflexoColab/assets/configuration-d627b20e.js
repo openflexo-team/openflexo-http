97f231108b7c27fcb49ea2c92edece5556f9ee16
Ihab Benamer <benamerihab25@gmail.com>
2023-08-22 13:19:34 +0200

    MEDIUM / fix lsp client socket connection

---
+++ http-server/src/main/resources/webroot/OpenflexoColab/assets/configuration-d627b20e.js
@@ -0,0 +1,4 @@
+import{uP as a,aQ as n}from"./index-e8d740ba.js";const e=".vscode",o="settings",I=`${e}/${o}.json`,i="vscode://schemas/settings/default",_="vscode://schemas/settings/user",A="vscode://schemas/settings/profile",h="vscode://schemas/settings/machine",r="vscode://schemas/settings/workspace",C="vscode://schemas/settings/folder",d="vscode://schemas/launch",N="vscode://schemas/tasks",m=[1],L=[2,3,4,5,6],S=[3,4,5],T=[1,...S],g=[2,3,4,5,6],P=[3,4,5,6],R=[4,5,6],s="tasks",t="launch",c=Object.create(null);c[s]=`${e}/${s}.json`;c[t]=`${e}/${t}.json`;const O=Object.create(null);O[s]=`${s}.json`;const l=a(n),F=`{
+	"version": "2.0.0",
+	"tasks": []
+}`;export{m as A,R as F,l as I,t as L,L as P,g as R,s as T,O as U,c as W,F as a,I as b,P as c,o as d,i as e,C as f,e as g,T as h,S as i,d as l,h as m,A as p,N as t,_ as u,r as w};
